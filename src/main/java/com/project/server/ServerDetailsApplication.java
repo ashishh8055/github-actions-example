fd2aedafdf74bc008513e504491d233b7a3105e4
ASHISH LAKSHKAR <ashishh8055@gmail.com>
2022-07-06 17:39:12 +0530

    made changes

--- src/main/java/com/project/server/ServerDetailsApplication.java
+++ src/main/java/com/project/server/ServerDetailsApplication.java
@@ -2,10 +2,17 @@ package com.project.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ServerDetailsApplication {
 
+	@GetMapping("/welcome")
+	public String welcome(){
+		return "Hello welcome to CI-CD sample application";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ServerDetailsApplication.class, args);
 	}
